d620133677db25f3116c6f267bdf1653e1069d42
ANDRES E ARGUEDAS L <ANDRES E ARGUEDAS L>
2017-04-19 16:38:43 -0600

    Acá va la base limpia y el script para la limpeiza, además de un par de modificaciones al script con funciones, en especial la función para asignar el ID

--- funciones.R
+++ funciones.R
@@ -1,50 +1,69 @@
-## Funciones para limpieza y procesamiento de la base ##
-
-# Mediante esto cargamos las libraries requeridas para las funciones.
-
-x <- c("stringr", "taRifx", "magrittr")
-lapply(x, require, character.only = T)
-remove(x)
-
-# Esta función identifica las fracciones en un vector de caracteres (en la forma
-# numero/numero y las transforma en un número)
-
-fraccion.a.numero <- function(x) {
-  if(is.character(x) == F) {
-    stop("El argumento ocupa ser un vector de caracteres")
-  }
-  temp <- strsplit(x, split = "/")
-  temp %<>% unlist()
-  temp %<>% destring()
-  return(temp[1] / temp[2])
-}
-
-# Esta función extrae todos los números, en forma numérica, que encuentra en un
-# vector de caracteres y devuelve un vector numérico con los valores. Aquí falta
-# hacer una función que transforme números escritos con letras en digitos y una
-# forma de lograr discriminar que números se deben escoger basados en la palabra
-# que le sigue, es decir, no tomar en cuenta el 3 en "durante 3 meses".
-
-extraer.numeros <- function(x) {
-  if(is.character(x) == F) {
-    stop("El argumento ocupa ser un vector de caracteres")
-  }
-  num <- str_extract_all(x, "\\(?[0-9,/]+\\)?")[[1]]
-  v <- length(grep("/", num))
-  if(length(v) != 0) {
-    for(i in 1:length(v)) {
-      num[v[i]] %<>% fraccion.a.numero()
+##Funciones para limpieza y procesamiento de la base##==========================
+  
+  # Mediante esto cargamos las libraries requeridas para las funciones.
+  
+  x <- c("stringr", "taRifx", "magrittr")
+  lapply(x, require, character.only = T)
+  remove(x)
+  
+  # Esta función identifica las fracciones en un vector de caracteres (en la 
+  # forma numero/numero y las transforma en un número)
+  
+  fraccion.a.numero <- function(x) {
+    if(is.character(x) == F) {
+      stop("El argumento ocupa ser un vector de caracteres")
     }
+    temp <- strsplit(x, split = "/")
+    temp %<>% unlist()
+    temp %<>% destring()
+    return(temp[1] / temp[2])
   }
-  return(destring(num, keep = "0-9./"))
-}
+  
+  # Esta función extrae todos los números, en forma numérica, que encuentra en 
+  # un vector de caracteres y devuelve un vector numérico con los valores. Aquí 
+  # falta hacer una función que transforme números escritos con letras en 
+  # digitos y una forma de lograr discriminar que números se deben escoger 
+  # basados en la palabra que le sigue, es decir, no tomar en cuenta el 3 en 
+  # "durante 3 meses".
+  
+  extraer.numeros <- function(x) {
+    if(is.character(x) == F) {
+      stop("El argumento ocupa ser un vector de caracteres")
+    }
+    num <- str_extract_all(x, "\\(?[0-9,/]+\\)?")[[1]]
+    v <- length(grep("/", num))
+    if(length(v) != 0) {
+      for(i in 1:length(v)) {
+        num[v[i]] %<>% fraccion.a.numero()
+      }
+    }
+    return(destring(num, keep = "0-9./"))
+  }
+  
+  # Esta función asigna un ID numérico único por cada elemento único de un 
+  # cierto vector. Aquí el argumento "x" sería la cedula y el argumento "y" el 
+  # nombre, para poder solucionar los casos de NA's. Cuando na.especial = T 
+  # entonces se le asigna un ID especifico a cada uno de los NA's, 
+  # na.especial = F entonces el argumento "y" no es necesario. La función 
+  # regresa un vector con los IDs.
+  
+  asignar.ID <- function(x, y, na.especial = T) {
+    ID <- match(x, unique(x))
+    if(na.especial) {
+      for(i in 1:length(unique(y[is.na(x)]))) {
+        IDt <- max(ID) + 1
+        if(length(unique(x[y == unique(y[is.na(x)])[i]])) == 1) {
+          ID[y == unique(y[is.na(x)])[i]] <- IDt
+        } else {
+          v <- (x[y == unique(y[is.na(x)])[i]])
+                 [!is.na(x[y == unique(y[is.na(x)])[i]])]
+          for(j in 1:length(v)) {
+            ID[x == v[j]] <- IDt
+          }
+        }
+      }
+    }
+    return(ID)
+  }
 
-# Esta función asigna un id numérico único por cada elemento único de un cierto
-# vector.
-
-asignar.id <- function(x) {
-  id <- match(x, unique(x))
-  return(id)
-}
-
-##FIN=========================================================================##
+##FIN##=========================================================================
---
+++ limpio.R
@@ -0,0 +1,122 @@
+##Limpieza de la base de clonazepam##===========================================
+  
+  # Primero, cargamos las librerías que ocupamos para este script:
+  
+  library(memisc)
+  library(magrittr)
+  library(lubridate)
+  
+  # Ahora, sourceamos las funciones necesarias para la limpieza de la base:
+  
+  source('F:/Proyecto enfermeria/Practica-Enfermeria/funciones.R')
+  
+  # Cargamos el archivo con las tabletas:
+  
+  tabletas <- as.data.set(spss.system.file(
+    'F:/practica enfermeria/I semestre/CLONAZEPANRE.sav'))
+  
+  # Arreglamos las variables de las bases para su uso más fácil:
+  
+  tabletas$id %<>% as.numeric()
+  tabletas$identificacion %<>% as.numeric()
+  tabletas$nombre %<>% as.character()
+  tabletas$paciente %<>% as.numeric()
+  tabletas$sexo %<>% as.factor()
+  tabletas$unidad.ejecutora %<>% as.numeric()
+  tabletas$edad %<>% as.numeric()
+  tabletas$producto %<>% as.character()
+  tabletas$descripcion %<>% as.factor()
+  tabletas$fecha %<>% dmy()
+  tabletas$fecha %<>% day()
+  tabletas$mes %<>% as.numeric()
+  tabletas$año %<>% as.numeric()
+  tabletas$medico %<>% as.character()
+  tabletas$especialidad %<>% as.character()
+  tabletas$indicacion %<>% as.character()
+  tabletas$dosis %<>% as.numeric()
+  tabletas$frecuencia %<>% as.numeric()
+  tabletas$presentacion %<>% as.factor()
+  tabletas$edadrec %<>% as.numeric()
+  tabletas$especificacion %<>% as.factor()
+  tabletas$mg %<>% as.numeric()
+  tabletas %<>% as.data.frame()
+  
+  # Ahora, cargamos la base con las gotas:
+  
+  gotas <- as.data.set(spss.system.file(
+    "F:/practica enfermeria/Clonazepán gotas/Clonagotas(1).sav"))
+  
+  # Y hacemos lo mismo que con las tabletas:
+  
+  gotas$v1 %<>% as.numeric()
+  options(scipen = 999)
+  gotas$identificacion %<>% as.numeric()
+  gotas$unidad %<>% as.numeric()
+  gotas$nombre %<>% as.character()
+  gotas$sexoc %<>% as.factor()
+  gotas$edad %<>% as.numeric()
+  gotas$producto %<>% as.character()
+  gotas$descripcion %<>% as.character()
+  gotas$fecha %<>% dmy()
+  gotas$fecha %<>% day()
+  gotas$mes %<>% as.numeric()
+  gotas$anno %<>% as.numeric()
+  gotas$cantidad %<>% as.numeric()
+  gotas$medico %<>% as.character()
+  gotas$especialidad %<>% as.character()
+  gotas$indicacion %<>% as.character()
+  gotas$indic0 %<>% as.character()
+  gotas %<>% as.data.frame()
+  
+  # Eliminamos las variables de la base de tabletas y gotas que no ocupamos y 
+  # creamos las variables nuevas que ocupamos:
+  
+  tabletas$id <- NULL
+  tabletas$edadrec <- NULL
+  gotas$v1 <- NULL
+  gotas$cantidad <- NULL
+  gotas$indic0 <- NULL
+  gotas$dosis <- 0
+  gotas$frecuencia <- 0
+  gotas$presentacion <- 0
+  gotas$especificacion <- 0
+  gotas$mg <- 0
+  gotas$paciente <- 0
+  
+  # Reordenamos las variables en la base de gotas y les cambiamos el nombre para 
+  # que sean iguales a las variables en la base de tabletas:  
+  
+  gotas <- gotas[c(1, 3, 19, 4, 2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 
+                   17, 18)]
+  names(gotas)[c(4, 5, 11)] <- names(tabletas)[c(4, 5, 11)]
+  cbind(names(gotas), names(tabletas))
+  names(gotas) == names(tabletas)
+  
+  # Ahora, juntamos ambas bases en una sola base:
+  
+  options(scipen = 999)
+  data <- rbind(gotas, tabletas)
+  data$nombre %<>% as.character()
+  data$fecha %<>% as.numeric()
+  data$descripcion %<>% as.character()
+  data$medico %<>% as.character()
+  data$especialidad %<>% as.character()
+  data$indicacion %<>% as.character()
+  data$presentacion %<>% as.factor()
+  data$especificacion %<>% as.factor()
+  
+  # Aplicamos la siguiente función para crear un ID único para cada cédula y 
+  # eliminamos las variables de nombre y cédula:
+  
+  data$ID <- asignar.ID(data$identificacion, data$nombre)
+  data$identificacion <- NULL
+  data$nombre <- NULL
+  data <- data[c(18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)]
+  
+  # Ahora, guardamos la base completa y limpia:
+  
+  dir.create("F:/Proyecto enfermeria/Practica-Enfermeria/data")
+  saveRDS(data, 
+        file = "F:/Proyecto enfermeria/Practica-Enfermeria/data/clonazepam.rds")
+
+##FIN##=========================================================================
