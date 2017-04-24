00d78584aea4fef497ed0112b4fa1e83e4f2ad1f
Andres Arguedas <andres.arguedas.leiva@gmail.com>
2017-04-23 22:16:07 -0600

    Hice grandes cambios a las funciones para manejar la dosis y la frecuencia, ahora una función regresa la dosis y la frecuencia y tiene un muy alto porcentaje de aciertos

--- funciones.R
+++ funciones.R
@@ -7,16 +7,54 @@
   remove(x)
   
   # Esta función identifica las fracciones en un vector de caracteres (en la 
-  # forma numero/numero y las transforma en un número)
+  # forma numero/numero) y las transforma en un número:
   
   fraccion.a.numero <- function(x) {
-    if(is.character(x) == F) {
-      stop("El argumento ocupa ser un vector de caracteres")
-    }
     temp <- strsplit(x, split = "/")
-    temp %<>% unlist()
-    temp %<>% destring()
-    return(temp[1] / temp[2])
+    for(i in 1:length(temp)) {
+      y <- temp[i]
+      y %<>% unlist()
+      y %<>% destring()
+      if(length(y) == 2) {
+        temp[i] <- y[1] / y[2]
+      } else {
+        temp[i] <- y
+      }
+    }
+    return(unlist(destring(temp)))
+  }
+  
+  # Esta función transforma los números escritos en un string a dígitos, para 
+  # poder ser procesados más fácilmente:
+  
+  texto.a.numero <- function(x) {
+    x %<>% tolower()
+    x %<>% gsub("una y media", "3/2", .)
+    x %<>% gsub("media", "1/2", .)
+    x %<>% gsub("un cuarto", "1/4", .)
+    x %<>% gsub("tres cuartos", "3/4", .)
+    x %<>% gsub("una y tres cuartos", "7/4", .)
+    x %<>% gsub("treinta y cinco", "35", .)
+    numeros <- list(cero = 0, uno = 1, una = 1, dos = 2, tres = 3, 
+                    cuatro = 4, cinco = 5, seis = 6, siete = 7, ocho = 8, 
+                    nueve = 9, diez = 10, once = 11, doce = 12, trece = 13, 
+                    catorce = 14, quince = 15, dieciseis = 16, diecisiete = 17, 
+                    dieciocho = 18, diecinueve = 19, veinte = 20, 
+                    ventiuno = 21, ventiun = 21, ventidos = 22, 
+                    ventitres = 23, venticuatro = 24, venticinco = 25, 
+                    ventiseis = 26, ventisiete = 27, ventiocho = 28, 
+                    ventinueve = 29,
+                    veintiuno = 21, veintiun = 21, veintidos = 22, 
+                    veintitres = 23, veinticuatro = 24, veinticinco = 25, 
+                    veintiseis = 26, veintisiete = 27, veintiocho = 28, 
+                    veintinueve = 29, treinta = 30)
+    wsplit <- unlist(strsplit(x, " "))
+    for(i in 1:length(wsplit)) {
+      if(wsplit[i] %in% names(numeros)) {
+        x %<>% gsub(wsplit[i], as.character(numeros[wsplit[i]]), .)
+      }
+    }
+    return(x)
   }
   
   # Esta función extrae todos los números, en forma numérica, que encuentra en 
@@ -26,18 +64,96 @@
   # basados en la palabra que le sigue, es decir, no tomar en cuenta el 3 en 
   # "durante 3 meses".
   
-  extraer.numeros <- function(x) {
+  extraer.numeros <- function(x, dosis = T, frecuencia = T) {
     if(is.character(x) == F) {
       stop("El argumento ocupa ser un vector de caracteres")
     }
-    num <- str_extract_all(x, "\\(?[0-9,/]+\\)?")[[1]]
-    v <- length(grep("/", num))
-    if(length(v) != 0) {
-      for(i in 1:length(v)) {
-        num[v[i]] %<>% fraccion.a.numero()
+    d <- 0
+    f <- 0
+    x %<>% gsub("tableta", "", ., ignore.case = T)
+    x %<>% gsub("media veces", " ", ., ignore.case = T)
+    x %<>% gsub("  ", " ", .)
+    x %<>% texto.a.numero()
+    x %<>% gsub("[(] \\(?[0-9/]+\\)? [)]", " ", .)
+    x %<>% gsub("[**].*", " ", .)
+    x %<>% gsub("[/][/].*", " ", .)
+    x %<>% gsub("[+][+].*", " ", .)
+    x %<>% gsub("[.][.].*", " ", .)
+    x %<>% gsub("[-][-].*", " ", .)
+    x %<>% gsub("[<].*", " ", .)
+    x %<>% gsub("bid", " 2 veces y ", .)
+    x %<>% gsub("tid", " 3 veces y ", .)
+    x %<>% gsub("c-\\(?[0-9/]+\\)?", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)? meses", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)?meses", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)? semanas", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)? dias", " ", .)
+    x %<>% gsub("llevo \\(?[0-9/]+\\)?", " ", .)
+    x %<>% gsub("retiro \\(?[0-9/]+\\)?", " ", .)
+    x %<>% gsub("hace \\(?[0-9/]+\\)?", " ", .)
+    x %<>% gsub("horas\\(?[0-9/]+\\)?", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)?d", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)?:\\(?[0-9/]+\\)?", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)? pm", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)? p.m.", " ", .)
+    x %<>% gsub("m4", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)?pm", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)?p.m.", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)? de la tarde", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)? cc", " ", .)
+    x %<>% gsub("\\(?[0-9/]+\\)? cucharada", " ", .)
+    x %<>% gsub(" las \\(?[0-9/]+\\)?", " ", .)
+    x %<>% gsub(" el \\(?[0-9/]+\\)?", " ", .)
+    x %<>% gsub("1 vez", " ", ., fixed = T)
+    x %<>% gsub(".y", " y", ., fixed = T)
+    x %<>% gsub("  ", " ", ., fixed = T)
+    for(i in 1:length(strsplit(x, split = " y ")[[1]])) {
+      y <- unlist(strsplit(x, split = " y "))[i]
+      k <- 1
+      ##AGREGAR SOLUCIÓN PARA PUNTOS ENTRE NÚMEROS##
+      if(length(grep("c/\\(?[0-9/]+\\)?", y)) > 0) {
+        k <- 24 / as.numeric(str_extract_all(str_extract_all(y, "c/\\(?[0-9]+\\)?")[[1]], 
+                                             "\\(?[0-9]+\\)?")[[1]])
+        y %<>% gsub("c/\\(?[0-9/]+\\)?", " ", .)
       }
+      if(length(grep("cada \\(?[0-9/]+\\)? horas", y)) > 0) {
+        k <- 24 / as.numeric(str_extract_all(str_extract_all(y, "cada \\(?[0-9/]+\\)? horas")[[1]], 
+                                             "\\(?[0-9/]+\\)?")[[1]])
+        y %<>% gsub("cada \\(?[0-9/]+\\)? horas", " ", .)
+      }
+      if(length(grep("\\(?[0-9/]+\\)? veces", y)) > 0) {
+        k <- as.numeric(str_extract_all(str_extract_all(y, "\\(?[0-9/]+\\)? veces")[[1]], 
+                                        "\\(?[0-9/]+\\)?")[[1]])
+        y %<>% gsub("\\(?[0-9/]+\\)? veces", " ", .)
+      }
+      y %<>% gsub("\\(?[0-9/]+\\)?h ", " ", .)
+      y %<>% gsub("\\(?[0-9/]+\\)?h[)]", " ", .)
+      num <- str_extract_all(y, "\\(?[0-9/]+\\)?")[[1]]
+      if(length(grep("/", num)) != 0) {
+        num %<>% fraccion.a.numero()
+      }
+      nums <- destring(num, keep = "0-9./")
+      d1 <- sum(nums)
+      f1 <- length(nums)
+      if(k > 1) {
+        f1 <- f1 * k
+        d1 <- d1 * f1
+      }
+      d <- d + d1
+      f <- f + f1
     }
-    return(destring(num, keep = "0-9./"))
+    if(!dosis & !frecuencia) {
+      return(nums)
+    }
+    if(dosis & !frecuencia) {
+      return(d)
+    }
+    if(!dosis & frecuencia) {
+      return(f)
+    }
+    if(dosis & frecuencia) {
+      return(c(d, f))
+    }
   }
   
   # Esta función asigna un ID numérico único por cada elemento único de un 
